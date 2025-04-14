cb7b208e553cc41d1f21b8dc3598b30b417dea87
Mosoveru <mosoveru@gmail.com>
2025-04-14 18:09:40 +0300

    refactor: remove unused service

--- webhook-server/src/services/recipients-resolver/recipients-resolver.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { GitWebhookServiceType } from '../../types';
-
-// @Injectable()
-// export class RecipientsResolverService {
-//   resolveRecipients(eventType: GitWebhookServiceType) {}
-// }
