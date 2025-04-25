377ad0303e119f970c0d3ffa6829dc53363dd185
Mosoveru <mosoveru@gmail.com>
2025-04-25 13:01:15 +0300

    refactor: combine decorators

--- webhook-server/src/decorators/event-payload.ts
+++ webhook-server/src/decorators/event-payload.ts
@@ -1,23 +1,46 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { WebhookEventPayload } from '../types';
-import { EventHeaders, RemoteGitServices } from '../constants/enums';
+import type { NullableEventPayload } from '../types';
+import { EventHeaders, RemoteGitServices, SecretTokenHeaders } from '../constants/enums';
+import { verify } from 'jsonwebtoken';
 
-export const EventPayload = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+export const Payload = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+  const jwtKey = process.env.PRIVATE_JWT_KEY;
+  if (!jwtKey) {
+    throw new Error('PRIVATE_JWT_KEY key is not defined in .env file');
+  }
   const req = ctx.switchToHttp().getRequest();
   const headers = req.headers;
   const body = req.body;
-  const gitServiceHeaders = new Map([
+  const eventPayload: NullableEventPayload<any> = {
+    service: null,
+    eventType: null,
+    eventPayload: null,
+    name: null,
+  };
+
+  const eventHeaders = new Map([
     [EventHeaders.GITLAB, RemoteGitServices.GITLAB],
     [EventHeaders.GITEA, RemoteGitServices.GITEA],
   ]);
-  for (const gitServiceEvent of gitServiceHeaders.keys()) {
-    if (headers[gitServiceEvent]) {
-      return {
-        service: gitServiceHeaders.get(gitServiceEvent)!,
-        eventType: headers[gitServiceEvent] as string,
-        eventPayload: body,
-      } satisfies WebhookEventPayload<any>;
+  const secretTokenHeaders = new Map([
+    [SecretTokenHeaders.GITLAB, RemoteGitServices.GITLAB],
+    [SecretTokenHeaders.GITEA, RemoteGitServices.GITEA],
+  ]);
+
+  for (const eventHeader of eventHeaders.keys()) {
+    if (headers[eventHeader]) {
+      eventPayload.service = eventHeaders.get(eventHeader)!;
+      eventPayload.eventType = headers[eventHeader] as string;
+      eventPayload.eventPayload = body;
     }
   }
-  return null;
+  for (const header of secretTokenHeaders.keys()) {
+    const serviceSecret = headers[header];
+    if (serviceSecret) {
+      eventPayload.name = verify(serviceSecret, jwtKey) as string;
+    }
+  }
+  const hasNull = Object.values(eventPayload).some((value) => value === null || value === undefined);
+
+  return hasNull ? null : eventPayload;
 });
