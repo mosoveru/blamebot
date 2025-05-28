aa5062a648dfa2fb88515bd0ef2792606df96fca
Mosoveru <mosoveru@gmail.com>
2025-05-28 12:44:28 +0300

    feat(bot): add service user entity and database interface

---
+++ bot/src/entities/service-user.entity.ts
@@ -0,0 +1,37 @@
+import { Column, Entity, OneToOne, PrimaryColumn, JoinColumn, ManyToOne } from 'typeorm';
+import { Service } from './service.entity';
+import { TelegramUser } from './telegram-user.service';
+
+@Entity({
+  name: 'service_users',
+})
+export class ServiceUser {
+  @PrimaryColumn()
+  serviceUserId: string;
+
+  @PrimaryColumn()
+  serviceId: string;
+
+  @Column()
+  telegramUserId: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  profileUrl: string;
+
+  @ManyToOne(() => TelegramUser)
+  @JoinColumn({
+    name: 'telegramUserId',
+    referencedColumnName: 'telegramUserId',
+  })
+  telegramUser: TelegramUser;
+
+  @OneToOne(() => Service)
+  @JoinColumn({ name: 'serviceId', referencedColumnName: 'serviceId' })
+  service: Service;
+}
--- bot/src/types/index.ts
+++ bot/src/types/index.ts
@@ -5,8 +5,33 @@ export interface GitRemoteApiHandler {
 }
 
 export type RemoteUserData = {
+  serviceUserId: string;
+  serviceId: string;
+  telegramUserId: string;
   username: string;
   email: string;
   profileUrl: string;
-  serviceUserId: string;
 };
+
+export type TelegramUserInfo = {
+  telegramUserId: string;
+  username: string;
+  name: string;
+};
+
+export type RemoteServiceInfo = {
+  serviceId: string;
+  remoteName: string;
+  gitProvider: string;
+  serviceUrl: string;
+};
+
+export interface DatabaseService {
+  saveTgUser(info: TelegramUserInfo): Promise<void>;
+  getTgUserInfo(id: string): Promise<TelegramUserInfo>;
+  saveRemoteUser(info: RemoteUserData): Promise<void>;
+  getRemoteUserInfo(serviceUserId: string, serviceId: string): Promise<RemoteUserData>;
+  saveRemoteServiceInfo(info: RemoteServiceInfo): Promise<void>;
+  getRemoteServiceInfo(serviceId: string): Promise<RemoteServiceInfo>;
+  findRemoteServiceInfo(url: string): Promise<RemoteServiceInfo>;
+}
