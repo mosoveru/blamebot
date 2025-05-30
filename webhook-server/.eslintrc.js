0d2d69c47506466d37a3ffacbbd042fc6a9aee7f
Mosoveru <mosoveru@gmail.com>
2025-05-30 10:49:26 +0300

    chore(webhook-server): add sonarjs eslint plugin

--- webhook-server/.eslintrc.js
+++ webhook-server/.eslintrc.js
@@ -5,10 +5,11 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'sonarjs'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:sonarjs/recommended-legacy'
   ],
   root: true,
   env: {
