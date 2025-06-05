d4e58c20db14e956bb65fbb8030a75d2ef31d3eb
Mosoveru <mosoveru@gmail.com>
2025-06-05 07:50:49 +0300

    feat(migrations): add migration and entity path

--- webhook-server/migrations/index.ts
+++ webhook-server/migrations/index.ts
@@ -1,13 +1,13 @@
 /**
  * @file Data Source file for typeorm migrations
- * Почему-то, TypeORM не может использовать путь в поле entities для поиска сущностей.
- * Пришлось прокидывать вручную, чтобы миграции могли быть сгенерированы с помощью migration:generate
  */
 
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
-import * as migrations from './migration-classes';
-import * as entities from '../src/models';
+import { join } from 'path';
+
+const entitiesPath = 'src/models';
+const migrationsPath = 'migrations/migration-classes';
 
 export const AppDataSource = new DataSource({
   type: process.env.DB_DRIVER as 'postgres',
@@ -16,7 +16,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER_LOGIN,
   password: process.env.DB_USER_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [...Object.values(entities)],
-  migrations: [...Object.values(migrations)],
+  entities: [join(__dirname, '..', entitiesPath, '*.entity{.ts,.js}')],
+  migrations: [join(__dirname, '..', migrationsPath, '*{.ts,.js}')],
   synchronize: false,
 });
--- webhook-server/migrations/migration-classes/index.ts
+++
@@ -1,1 +0,0 @@
-export { InitDatabase1749049920094 } from './1749049920094-InitDatabase';
